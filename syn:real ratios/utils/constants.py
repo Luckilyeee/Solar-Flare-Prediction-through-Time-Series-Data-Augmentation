ca3977223b8d48bd7c2dddda0fa8e6ad0acdaae3
lipeiyu <60626754+Luckilyeee@users.noreply.github.com>
2025-03-17 18:51:37 -0600

    Update constants.py

--- syn:real ratios/utils/constants.py
+++ syn:real ratios/utils/constants.py
@@ -1,17 +1,19 @@
-ITERATIONS = 5 # nb of random runs for random initializations
-
-
+# for ratio study
+ITERATIONS = 5
 DATASET_NAMES = ['solarflare']
-
-sizes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+sizes = [0, 1, 2, 3, 4, 5, 6]
 
 CLASSIFIERS = ['fcn']
-ARCHIVE_NAMES = ['SF_aug_ws']
-DATA_FOLDERS = {'time_warp', 'window_slice'}
-data_folders_for_archive = {'SF_aug_ws': 'window_slice', 'SF_aug_tw': 'time_warp'}
+ARCHIVE_NAMES = ['SF_aug_permutation']
+DATA_FOLDERS = {'permutation'}
+data_folders_for_archive = {'SF_aug_permutation': 'permutation'}
 
+CLASSIFIERS = ['resnet']
+ARCHIVE_NAMES = ['SF_aug_permutation']
+DATA_FOLDERS = {'permutation'}
+data_folders_for_archive = {'SF_aug_permutation': 'permutation'}
 
-# CLASSIFIERS = ['resnet', 'lstm_fcn']
-# ARCHIVE_NAMES = ['SF_aug_tw']
-# DATA_FOLDERS = {'time_warp', 'window_slice'}
-# data_folders_for_archive = {'SF_aug_ws': 'window_slice', 'SF_aug_tw': 'time_warp'}
+CLASSIFIERS = ['lstm_fcn']
+ARCHIVE_NAMES = ['SF_aug_spawner']
+DATA_FOLDERS = {'SPAWNER'}
+data_folders_for_archive = {'SF_aug_spawner': 'SPAWNER'}
