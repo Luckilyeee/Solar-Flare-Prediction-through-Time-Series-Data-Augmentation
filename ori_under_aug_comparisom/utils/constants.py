e5d1c5da240fcdc1f59fa603d9d957ed812a7871
lipeiyu <60626754+Luckilyeee@users.noreply.github.com>
2025-03-17 19:08:09 -0600

    Update constants.py

--- ori_under_aug_comparisom/utils/constants.py
+++ ori_under_aug_comparisom/utils/constants.py
@@ -18,5 +18,19 @@ data_folders_for_archive = {'SF_real_ori': 'Ori_data'}
 #
 # data_folders_for_archive = {'SF_real_under': 'train_under'}
 
+# for augmentation on stratified data
+# ITERATIONS = 5
+# DATASET_NAMES = ['solarflare']
+# CLASSIFIERS = ['resnet']
+# ARCHIVE_NAMES = ['SF_aug_ww', 'SF_aug_ws', 'SF_aug_wdba', 'SF_aug_tw', 'SF_aug_spawner', 'SF_aug_rgw', 'SF_aug_mw', 'SF_aug_dgw', 'SF_aug_permutation', 'SF_aug_scaling', 'SF_aug_jitter', 'SF_aug_rotation']
+# DATA_FOLDERS = {'discriminative_guided_warp', 'random_guided_warp', 'time_warp',
+#                'window_slice', 'magnitude_warp', 'SPAWNER', 'wdba', 'window_warp', 'permutation', 'scaling', 'jitter', 'rotation'}
+
+# data_folders_for_archive = {'SF_aug_ww': 'window_warp', 'SF_aug_wdba': 'wdba', 'SF_aug_spawner': 'SPAWNER',
+# 'SF_aug_ws': 'window_slice', 'SF_aug_rgw': 'random_guided_warp', 'SF_aug_mw': 'magnitude_warp',
+# 'SF_aug_tw': 'time_warp', 'SF_aug_dgw': 'discriminative_guided_warp',
+# 'SF_aug_permutation': 'permutation', 'SF_aug_scaling': 'scaling', 'SF_aug_jitter': 'jitter', 'SF_aug_rotation': 'rotation'}
+
+
 
 
