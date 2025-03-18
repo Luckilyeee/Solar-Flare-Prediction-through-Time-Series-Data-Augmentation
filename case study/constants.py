d104cd063a0d9514c4428af427817377fac5e93d
lipeiyu <60626754+Luckilyeee@users.noreply.github.com>
2025-03-17 19:23:49 -0600

    Create constants.py

---
+++ case study/constants.py
@@ -0,0 +1,26 @@
+# for four class study ori
+# ITERATIONS = 10
+# DATASET_NAMES = ['solarflare']
+# CLASSIFIERS = ['fcn', 'resnet', 'lstm_fcn']
+# ARCHIVE_NAMES = ['SF_four_class']
+# DATA_FOLDERS = {'Ori_four'}
+# data_folders_for_archive = {'SF_four_class': 'Ori_four'}
+
+# for four class study aug
+# ITERATIONS = 10
+# DATASET_NAMES = ['solarflare']
+
+# CLASSIFIERS = ['fcn']
+# ARCHIVE_NAMES = ['SF_aug_permutation']
+# DATA_FOLDERS = {'permutation'}
+# data_folders_for_archive = {'SF_aug_permutation': 'permutation'}
+
+# CLASSIFIERS = ['lstm_fcn']
+# ARCHIVE_NAMES = ['SF_aug_spawner']
+# DATA_FOLDERS = {'SPAWNER'}
+# data_folders_for_archive = {'SF_aug_spawner': 'SPAWNER'}
+#
+# CLASSIFIERS = ['resnet']
+# ARCHIVE_NAMES = ['SF_aug_permutation']
+# DATA_FOLDERS = {'permutation'}
+# data_folders_for_archive = {'SF_aug_permutation': 'permutation'}
